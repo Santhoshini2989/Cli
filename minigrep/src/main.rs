1001be1107fcd7e5dd73b80df21af2230a5bb09d
santhoshini <santhoshinia01@gmail.com>
2026-04-25 09:39:50 +0530

    added lib.rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
+use minigrep::search;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,10 +15,22 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("should have been able to read the file");
+   if let Err(e)=run(config){
+    println!("Application error:{e}");
+    process::exit(1);
+   }
+}
 
-    println!("with text:\n{contents}");
+
+fn run(config:Config)->Result<(),Box<dyn Error>>{
+   
+   let contents =
+        fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query,&contents){
+        println!("{line}");
+    }
+  Ok(())
 }
 
 struct Config {
